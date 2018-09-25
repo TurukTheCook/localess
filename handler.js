5c10f1ea2c7d30f48ae73a135758821d53f3dc8e
TurukTheCook <yoann.roule@gmail.com>
2018-09-25 13:36:48 +0200

    init

--- handler.js
+++ handler.js
@@ -2,19 +2,14 @@
 
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
+const elasticsearch = require('elasticsearch');
 const dd = new AWS.DynamoDB.DocumentClient({
   region: 'localhost',
   endpoint: 'http://localhost:8000'
 });
-const cs = new AWS.CloudSearchDomain({
-  region: 'localhost',
-  endpoint: 'http://localhost:15808',
-  apiVersion: '2013-01-01'
-});
-const cs2 = new AWS.CloudSearchDomain({
-  region: 'localhost',
-  endpoint: 'http://localhost:15808?format=sdk',
-  apiVersion: '2013-01-01'
+const es = new elasticsearch.Client({
+  host: 'localhost:4571',
+  // log: 'trace'
 });
 const TABLE_NAME = "usersTable";
 
@@ -42,10 +37,7 @@ module.exports.PutItem = (event, context, callback) => {
       })
       .catch(error => {
         console.error(error);
-        callback(null, {
-          statusCode: error.statusCode || 501,
-          message: 'Could not add user'
-        });
+        callback(null, "Couldn't add user to ddb..");
       });
 };
 
@@ -67,80 +59,89 @@ module.exports.DeleteItem = (event, context, callback) => {
     })
     .catch(error => {
       console.log(error);
-      callback(null, {
-        statusCode: error.StatusCode || 501,
-        message: 'Could not delete user'
-      });
+      callback(null, "Couldn't delete user from ddb..");
     });
 };
 
 module.exports.Scan = (event, context, callback) => {
   dd.scan({TableName: TABLE_NAME}).promise()
-    .then(result => {
-      callback(null, result);
+    .then(res => {
+      callback(null, res);
     })
-    .catch(error => {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        message: 'Could not scan'
-      });
+    .catch(err => {
+      console.error(err);
+      callback(null, "Could'nt scan ddb..");
     });
 };
 
 /**
- * DYNAMODB-STREAMS AND CLOUDSEARCH
+ * DYNAMODB-STREAMS AND ELASTICSEARCH
  */
  
 module.exports.triggerStream = (event, context, callback) => {
   console.log('DDB Stream Triggered..')
+  
   let record = event.Records[0];
-
   let data = {id : record.dynamodb.Keys.userId.S};
   let image = record.dynamodb.NewImage;
+  let params = {
+    index: 'users',
+    type: 'user',
+    id: data.id
+  };
 
   // Delete record deleted from DynamoDB or fill data fields
   if (record.eventName === 'REMOVE') {
-    data.type = 'delete';
-    data.version = 1;
+    es.delete(params)
+      .then(res => {
+        callback(null, res);
+      })
+      .catch(err => {
+        console.error(err);
+        callback(null, 'Could not remove item from ES..');
+      });
   } else {
-    data.type = 'add';
-    data.version = 1;
-    data.fields = {
+    params.body = {
       name: image.name.S,
-      lastname: image.lastname.S,
+      lastname: image.lastname.S, 
     };
+    es.create(params)
+      .then(res => {
+        callback(null, res);
+      })
+      .catch(err => {
+        console.error(err);
+        callback(null, 'Could not add item to ES..');
+      });
   }
-
-  let documents = [data];
-  let params = {
-    contentType: 'application/json',
-    documents: JSON.stringify(documents)
-  };
-
-  cs.uploadDocuments(params, (err, data) => {
-    if (err) console.error(err);      // an error occurred
-    else console.log(data);           // successful response
-  });
 };
 
 module.exports.Search = (event, context, callback) => {
+  let data = JSON.parse(JSON.stringify(event));
   let params = {
-    query: event,
-    size: 100,
-    start: 0,
+    q: data.q,
+    size: data.size,
+    from: data.from,
   };
   console.log(params);
-  
-  cs2.search(params).promise()
-    .then(result => {
-      callback(null, result);
+
+  es.search(params)
+    .then(res => {
+      callback(null, res);
     })
-    .catch(error => {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        message: 'Could not search'
-      });
+    .catch(err => {
+      callback(null, err);
     });
+};
+
+module.exports.PingEs = (event, context, callback) => {
+  es.ping({
+    requestTimeout: 30000,
+    }, err => {
+      if (err) {
+        console.error('elasticsearch cluster is down!');
+      } else {
+        console.log('All is well');
+      }
+  });
 };
